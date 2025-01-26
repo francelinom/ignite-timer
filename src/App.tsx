2446dc862ebfe2478f7c4ada4cccdbfabf32694d
francelino <fmarcilio1989@gmail.com>
2025-01-26 13:35:45 -0300

    Feat: creating Router component and removing button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
 import { defaultTheme } from "./styles/themes/detault"
+import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom"
 import { GlobalStyle } from "./styles/global"
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button variant="warning" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
