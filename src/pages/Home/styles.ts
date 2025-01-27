37f98d8b4a4dae6be6aaac377bba4ad35d2fe907
francelino <fmarcilio1989@gmail.com>
2025-01-27 08:54:56 -0300

    Feat: style button

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 export const HomeContainer = styled.main`
   flex: 1;
   display: flex;
@@ -12,6 +13,7 @@ export const HomeContainer = styled.main`
     gap: 3.5rem;
   }
 `;
+
 export const FormContainer = styled.div`
   width: 100%;
   display: flex;
@@ -23,6 +25,33 @@ export const FormContainer = styled.div`
   font-weight: bold;
   flex-wrap: wrap;
 `;
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
 export const CountdownContainer = styled.div`
   font-family: "Roboto Mono", monospace;
   font-size: 10rem;
@@ -36,6 +65,7 @@ export const CountdownContainer = styled.div`
     border-radius: 8px;
   }
 `;
+
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme["green-500"]};
@@ -43,4 +73,26 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
 `;
