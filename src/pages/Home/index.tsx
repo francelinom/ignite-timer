03356383facb279d1f06953fa057418f0db523d0
francelino <fmarcilio1989@gmail.com>
2025-01-28 11:16:32 -0300

    Feat: adding countdown timer to timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import { Play } from 'phosphor-react'
-import { useForm } from 'react-hook-form'
+import { set, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState, useEffect } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 import {
     CountdownContainer,
@@ -13,6 +15,7 @@ import {
     TaskInput
 } from "./styles"
 
+
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Informe a tarefa'),
     minutesAmount: zod
@@ -23,7 +26,18 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cyrcle {
+    id: string
+    task: string
+    minutesAmount: number
+    startDate: Date
+}
+
 export function Home() {
+    const [cyrcle, setCyrcle] = useState<Cyrcle[]>([])
+    const [activeCyrcleId, setCyrcleId] = useState<string | null>(null)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
     /**
      * formState -> tem a funcionalidade de pegar o erro que é passado pelo resolver  "errors"
      * Utilização:
@@ -37,12 +51,45 @@ export function Home() {
         }
     });
 
-    const task = watch('task')
-    const isSubmitDisabled = !task
+    const activeCyrcle = cyrcle.find((cyrcle) => cyrcle.id === activeCyrcleId)
+
+    useEffect(() => {
+        if (activeCyrcle) {
+            setInterval(() => {
+                setAmountSecondsPassed(
+                    differenceInSeconds(new Date(), activeCyrcle.startDate)
+                )
+            }, 1000)
+        }
+    }, [activeCyrcle])
 
     function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime());
+        const newCyrcle: Cyrcle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+            startDate: new Date()
+        }
+
+        setCyrcle(((state) => [...state, newCyrcle]))
+        setCyrcleId(id)
         reset()
     }
+
+
+
+    const totalSeconds = activeCyrcle ? activeCyrcle.minutesAmount * 60 : 0
+    const currentSeconds = activeCyrcle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
 
     return (
         <HomeContainer>
@@ -77,11 +124,11 @@ export function Home() {
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <StartCountdownButton disabled={isSubmitDisabled} type="submit">
